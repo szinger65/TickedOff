c10a1042a30bfb047f94465a90493b4ef59e372e
Solaiman <solaimanzinger@icloud.com>
2026-01-12 21:56:58 +0100

    bug fixes

--- src/pages/login.js
+++ src/pages/login.js
@@ -69,7 +69,7 @@ export default function Login() {
           </Link>
         </div>
       </form>
-      <div className="absolute bottom-6 text center">
+      <div className="absolute bottom-6 text-lg center">
         <Link to="/about" className="text-blue-600 font-semibold hover:underline">
           About
         </Link>
