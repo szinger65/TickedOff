cae511cfb5b2870500b3778ad435ffae8668967f
Solaiman <solaimanzinger@icloud.com>
2026-01-16 19:42:25 +0100

    password verification and background ui

--- src/pages/login.js
+++ src/pages/login.js
@@ -6,6 +6,8 @@ import { Input } from "../components/ui/input";
 
 import SplitText from '../components/ui/splittext';
 
+import LiquidEther from '../components/ui/liquidether';
+
 
 export default function Login() {
   const [email, setEmail] = useState('');
@@ -30,6 +32,25 @@ export default function Login() {
 
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-slate-50">
+      <div style={{ width: '100%', height: 600, position: 'relative' }}>
+        <LiquidEther
+          colors={[ '#5227FF', '#FF9FFC', '#B19EEF' ]}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous={false}
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo={true}
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+        />
+      </div>
       <div className="mb-10 text-center">
         <SplitText 
           text="TickedOff" 
